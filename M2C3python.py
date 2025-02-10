1172c5cc3c7d3aab74ab1d4357f2159a3ac03298
DaniUL99 <83228977+DaniUL99@users.noreply.github.com>
2025-02-10 17:42:17 +0100

    Add files via upload

---
+++ M2C3python.py
@@ -0,0 +1,68 @@
+
+# Exercise 1
+
+string = "This is a string."
+
+number = 5
+
+list = ('magpie', 'blackbird', 'robin', 'crow')
+
+boolean: True
+
+# Exercise 2
+
+new_string = string[:3]
+
+print(new_string)
+
+# Exercise 3
+
+first_element_list = list[0]
+
+print(first_element_list)
+
+# Exercise 4
+
+new_number = number + 10
+
+print(new_number)
+
+# Exercise 5
+
+last_element_list = list[-1]
+
+print(last_element_list)
+
+# Exercise 6
+
+names = 'harry, alex, susie, jared, gail, conner'
+
+names = names.split(', ')
+
+print(names)
+
+# Exercise 7
+
+first_word = string[0:4].upper()
+
+exercise_7 = first_word + string[4:]
+
+print(exercise_7)
+
+# Exercise 8
+
+sentence = f"I think {number} is a neat number."
+
+print(sentence)
+
+# Exercise 9
+
+print("hello world")
+
+# Exercise 10
+
+extra = "¡Hola mundo!"
+
+extra = extra.replace('Hola', 'Adiós')
+
+print(extra)
